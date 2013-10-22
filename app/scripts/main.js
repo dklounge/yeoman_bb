a7774ccc9ce2a830e8936513748e7fe29f94dce2
dklounge <daviddarden11@gmail.com>
2013-10-22 00:02:33 -0700

    yeoman first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global yeomanBb, $*/
+
+
+window.yeomanBb = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    yeomanBb.init();
+});
